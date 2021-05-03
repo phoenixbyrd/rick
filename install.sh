9b3fb9821e28af4c2d229f582218b50caff1c08c
phoenixbyrd <james.gundlach79@gmail.com>
2021-05-03 18:51:29 -0400

    never gonna give you up

--- install.sh
+++ install.sh
@@ -4,15 +4,19 @@ sudo mv rick.sh /opt/rick
 sudo mv roll.sh /opt/rick
 sudo mv rick.png /opt/rick
 sudo mv .git/ /opt/rick
+sudo mv install.sh /opt/rick
+sudo mv README.md /opt/rick
+sudo mv Rickroll.desktop /opt/rick
 
 sudo chown $USER:$USER /opt/rick/
 sudo chown $USER:$USER /opt/rick/rick.png
 sudo chown $USER:$USER /opt/rick/rick.sh
 sudo chown $USER:$USER /opt/rick/roll.sh
 sudo chown $USER:$USER /opt/rick/.git/
+sudo chown $USER:$USER /opt/rick/install.sh
+sudo chown $USER:$USER /opt/rick/README.md
+sudo chown $USER:$USER /opt/rick/Rickroll.desktop
 
 cp Rickroll.desktop ~/.local/share/applications
-
-cd ~ && rm -r ~/rick
 
 notify-send -i /opt/rick/rick.png --urgency=low --app-name="" Rickroll Installed
